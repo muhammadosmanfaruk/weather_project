52955393af108be8d5b7ae5a1bbec948bb742182
Mohammad Osman Faruq <muhammadosmanfaruk@gmail.com>
2021-12-05 16:42:29 +0600

    Weather

--- lib/current_weather.dart
+++ lib/current_weather.dart
@@ -27,7 +27,13 @@ class _CurrentWeatherState extends State<CurrentWeather> {
   Widget build(BuildContext context) {
     return Consumer<WeatherProvider>(
         builder: (context, value, child) =>isLoading? Center(child:CircularProgressIndicator()) :
-          Center(child: Text("${value.getWeatherData.main.temp}"),
+          Container(
+            height: 300,
+            width: MediaQuery.of(context).size.width - 20,
+            decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+            ),
+            child: Text("${value.getWeatherData.main.temp} ${value.getWeatherData.name}"),
         ),
     );
 
